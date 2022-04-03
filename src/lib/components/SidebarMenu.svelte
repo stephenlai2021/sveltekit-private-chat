91f1a0a637e487797a61959bdf708048089cf520
Stephen_Lai <stephenlai2021@gmail.com>
2022-04-03 11:09:24 +0800

    commit

--- src/lib/components/SidebarMenu.svelte
+++ src/lib/components/SidebarMenu.svelte
@@ -1,7 +1,6 @@
 <script>
   import { auth } from "$lib/firebase/client";
   import { onAuthStateChanged } from "firebase/auth";
-  // import { collection, onSnapshot } from "firebase/firestore";
   import {
     mobile,
     bgColor,
@@ -9,51 +8,23 @@
     showSettingsModal,
   } from "$lib/store";
   import { page } from "$app/stores";
-  // import { goto } from "$app/navigation";
-  // import { activeItem } from "$lib/store";
 
   let user = null;
-  // let users = null;
-  // let colRef = collection(db, "whatzapp_users");
-  // let activeMenu = menuIcon[0];
-  // let menuIcon = ["sms", "volunteer_activism", "movie"];
-  // let menuIcon = [
-  //   "home-outline",
-  //   "chatbox-ellipses-outline",
-  //   "heart-circle-outline",
-  //   "film-outline",
-  // ];
 
   onAuthStateChanged(auth, (_user) => (user = _user));
-
-  // const unsub = onSnapshot(colRef, (snapshot) => {
-  //   let tempUsers = [];
-  //   snapshot.docs.forEach((doc) => {
-  //     tempUsers.push({ ...doc.data() });
-  //   });
-  //   users = tempUsers;
-  //   return () => unsub();
-  // });
-
-  // const switchRoute = (item) => {
-  //   if (item === "home-outline") goto(`/`);
-  //   if (item === "chatbox-ellipses-outline") goto(`/${users[0].name}`);
-  //   if (item === "heart-circle-outline") goto("/tinder");
-  //   if (item === "film-outline") goto("/movie");
-  // };
 </script>
 
+<!-- style:background={$bgColor} -->
   <div
   class="sidebar-menu"
   class:loginform-hide={$loginFormShow}
-  style:background={$bgColor}
+  style:background={$page.url.pathname === "/" ? "#cccccc" : $bgColor}
   style:display={$mobile
     ? "none"
     : $page.url.pathname === "/login"
     ? "none"
     : "flex"}
 >
-<!-- style:background={$page.url.pathname === "/" ? "#F8F8F8" : $bgColor} -->
   {#if user}
     <div
       class="userimg"
@@ -77,28 +48,6 @@
     right: -5px;
     font-size: 1.2em;
   }
-
-  /* .menu-item:active {
-    background: rgba(0, 0, 0, 0.1);
-  }
-
-  .menu-item:hover {
-    background: rgba(0, 0, 0, 0.1);
-  }
-
-  .menu-item {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-    cursor: pointer;
-    padding: 8px;
-    border-radius: 10px;
-  } */
-
-  /* span {
-    font-size: 1.5em;
-  } */
-
   .userimg img {
     width: 40px;
     height: 40px;
@@ -113,8 +62,6 @@
     position: relative;
     cursor: pointer;
     margin-top: auto;
-    /* margin: auto 10px 0 10px; */
-    /* border: 1px solid; */
   }
 
   .sidebar-menu {
@@ -137,11 +84,9 @@
       z-index: 200;
       justify-content: space-around;
     }
+    
     .userimg {
       display: none;
-    }
-    .menu-item {
-      margin-bottom: 0;
     }
   }
 </style>
