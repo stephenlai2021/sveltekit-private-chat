9e7dc608222ddb2765bba5f2e9eacb434b5a5338
Stephen_Lai <stephenlai2021@gmail.com>
2022-08-04 18:32:42 +0800

    commit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -137,7 +137,7 @@
 </svelte:head>
 
 <!-- <SvelteTheme enableSystem={false} forcedTheme={'light'} /> -->
-<SvelteTheme defaultTheme={'light'} />
+<SvelteTheme enableSystem={false} defaultTheme={'light'} />
 <div class="wrapper" on:click={closeModal}>
   <div
     class="inner-wrapper"
