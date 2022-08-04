c564e2a14a6c7d885b34cd560ce628c19cbcce0f
Stephen_Lai <stephenlai2021@gmail.com>
2022-08-04 17:47:08 +0800

    commit

--- src/lib/store/index.js
+++ src/lib/store/index.js
@@ -99,18 +99,11 @@ currentSelectedUser.subscribe(
   (val) => browser && localStorage.setItem("selected user", val)
 );
 
-export const isDarkMode = writable(
-  browser && localStorage.getItem("mode") || false
+export const selectedUsername = writable(
+  browser && localStorage.getItem("selected user name") || {}
 );
 selectedUsername.subscribe(
-  (val) => browser && localStorage.setItem("mode", val)
-);
-
-export const mode = writable(
-  browser && localStorage.getItem("mode") || "light"
-);
-selectedUsername.subscribe(
-  (val) => browser && localStorage.setItem("mode", val)
+  (val) => browser && localStorage.setItem("selected user name", val)
 );
 
 export const selectedUsername = writable(
