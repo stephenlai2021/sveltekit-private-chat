cb5470c63cac7878a43144e7f433572963367589
Stephen_Lai <stephenlai2021@gmail.com>
2022-08-27 22:22:01 +0800

    commit

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -2,15 +2,21 @@
   import Login from "$lib/components/Login.svelte";
   import { auth } from "$lib/firebase/client";
   import { onAuthStateChanged } from "firebase/auth";
-  import { goto } from '$app/navigation'
-  import { onMount } from 'svelte'
-  
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+  import { browser } from "$app/env";
+
+  browser &&
+    onAuthStateChanged(auth, (user) => {
+      if (user) goto("/");
+    });
+
   onMount(() => {
-    // onAuthStateChanged(auth, user => { 
+    // onAuthStateChanged(auth, user => {
     //   if (user) goto('/')
     // })
-    return () => location.reload() 
-  })
+    return () => location.reload();
+  });
 </script>
 
 <svelte:head>
